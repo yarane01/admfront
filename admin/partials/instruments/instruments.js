266849be09c65571b6af5e2af337678ba94fd463
Alexander Lepekhin <alepekhine@gmail.com>
2020-02-17 15:50:49 +0300

    edit instrument with comment

--- admin/partials/instruments/instruments.js
+++ admin/partials/instruments/instruments.js
@@ -52,7 +52,7 @@ instrumentsControllers.controller('InstrumentsCtrl',
                             title: 'Instruments',
                             exportOptions: {
                                 //orthogonal: 'export',
-                                columns: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ]
+                                columns: [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ]
                             }
                         }
                     ],
